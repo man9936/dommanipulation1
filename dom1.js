39b559fa5b71cddfec00c2842544cc66dc146e56
man9936 <112785474+man9936@users.noreply.github.com>
2022-09-22 15:40:37 +0530

    Update dom1.js

--- dom1.js
+++ dom1.js
@@ -4,3 +4,27 @@
 console.log(document.forms);
 console.log(document.title);
 let title=document.getElementsByTagName("title");
+
+//console.log(document.body);
+//console.log(document.head);
+//console.log(document.forms);
+//console.log(document.title);
+let title=document.getElementById("header-title");
+//console.log(title.textContent='hello');
+/*
+text content changes the whole text content irrespective of style of output
+and html
+innerText takes care of style
+innerHTML adds changes through HTML format to the file instead of replacing it.
+*/
+let header=document.getElementById("main-header");
+header.style.borderBottom='solid 5px #000';
+title.innerHTML='<b>Item Lister</b>'
+header.style.borderTop='solid 5px #000';
+header.style.borderLeft='solid 5px #000';
+header.style.borderLeft='solid 5px #000';
+title.style.background='black';
+title.style.color='green';
+
+
+
