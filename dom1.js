ec6f86facdb318662deb38d1a24347178bfa8bc6
man9936 <112785474+man9936@users.noreply.github.com>
2022-09-22 16:35:01 +0530

    dom2.js

--- dom1.js
+++ dom1.js
@@ -1,12 +1,6 @@
 
 //console.log(document.body);
 //console.log(document.head);
-console.log(document.forms);
-console.log(document.title);
-let title=document.getElementsByTagName("title");
-
-//console.log(document.body);
-//console.log(document.head);
 //console.log(document.forms);
 //console.log(document.title);
 let title=document.getElementById("header-title");
@@ -17,6 +11,7 @@ and html
 innerText takes care of style
 innerHTML adds changes through HTML format to the file instead of replacing it.
 */
+/*
 let header=document.getElementById("main-header");
 header.style.borderBottom='solid 5px #000';
 title.innerHTML='<b>Item Lister</b>'
@@ -25,6 +20,23 @@ header.style.borderLeft='solid 5px #000';
 header.style.borderLeft='solid 5px #000';
 title.style.background='black';
 title.style.color='green';
+let list=document.getElementById("items");
+console.log(list.childElementCount);
+
+*/
+let items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+/*
+items[2].textContent='helloyoutoo';
+items[2].style.fontWeight='bold';
+
+*/
+for(var i=0;i<items.length ; i++)
+{
+    items[i].style.fontWeight='bold';
+}
+items[2].style.backgroundColor='green';
 
 
 
