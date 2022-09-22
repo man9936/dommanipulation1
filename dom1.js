d0faea0053d78f134ec2c41e286a4a5e3c3c9b45
man9936 <mk9934dude@gmail.com>
2022-09-22 15:05:25 +0530

    third commit

--- dom1.js
+++ dom1.js
@@ -1,8 +1,6 @@
-console.log("hello git");
-console.log("whats' up?");
-console.log("whats' up?");
-console.log("whats' up?");
-console.log("whats' up?");
-console.log("whats' up?");
-document.title="new";
 
+//console.log(document.body);
+//console.log(document.head);
+console.log(document.forms);
+console.log(document.title);
+let title=document.getElementsByTagName("title");
